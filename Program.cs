73e0964b8ea3049a23244e9fa4ffe651d6374883
Dmitriy <35215775+DimaTer89@users.noreply.github.com>
2023-06-20 14:56:42 +0300

    update solution and add read.me

--- Program.cs
+++ Program.cs
@@ -5,7 +5,13 @@
 		static void Main(string[] args)
 		{
 			string[] firstLine = Console.ReadLine().TrimEnd().Split(' ');
-			
-		}
+			int n1 = Convert.ToInt32(firstLine[0]);
+			int n2 = Convert.ToInt32(firstLine[1]);
+			int n3 = Convert.ToInt32(firstLine[2]);
+			List<int> h1 = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(val => Convert.ToInt32(val)).ToList();
+			List<int> h2 = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(val => Convert.ToInt32(val)).ToList();
+			List<int> h3 = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(val => Convert.ToInt32(val)).ToList();
+            Console.WriteLine(StacksEqual.EqualStacks(h1, h2, h3));
+        }
 	}
 }
--- StacksEqual.cs
+++ StacksEqual.cs
@@ -1,21 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EqualStacks
+﻿namespace EqualStacks
 {
 	internal class StacksEqual
 	{
+		private static int GetSumCylinder(List<int> arr, Stack<int> st)
+		{
+			int sumCylinder = 0;
+			for (int i = arr.Count - 1; i >= 0; --i)
+			{
+				sumCylinder += arr[i];
+				st.Push(arr[i]);
+			}
+			return sumCylinder;
+		}
+
 		public static int EqualStacks(List<int> h1, List<int> h2, List<int> h3)
 		{
-			int sumSt1 = h1.Sum();
-			int sumSt2 = h2.Sum();
-			int sumSt3 = h3.Sum();
-			Stack<int> st1 = new(h1);
-			Stack<int> st2 = new(h2);
-			Stack<int> st3 = new(h3);
+			Stack<int> st1 = new();
+			Stack<int> st2 = new();
+			Stack<int> st3 = new();
+			int sumSt1 = GetSumCylinder(h1, st1);
+			int sumSt2 = GetSumCylinder(h2, st2);
+			int sumSt3 = GetSumCylinder(h3, st3);
 			while (sumSt1 != sumSt2 || sumSt2 != sumSt3)
 			{
 				if (sumSt1 > sumSt2 && sumSt1 > sumSt3)

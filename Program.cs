32dd520e9f8fcc85fac88e37449e43a7f9071e18
Dmitriy <35215775+DimaTer89@users.noreply.github.com>
2023-06-20 14:32:52 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EqualStacks
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] firstLine = Console.ReadLine().TrimEnd().Split(' ');
+			
+		}
+	}
+}
---
+++ StacksEqual.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EqualStacks
+{
+	internal class StacksEqual
+	{
+		public static int EqualStacks(List<int> h1, List<int> h2, List<int> h3)
+		{
+			int sumSt1 = h1.Sum();
+			int sumSt2 = h2.Sum();
+			int sumSt3 = h3.Sum();
+			Stack<int> st1 = new(h1);
+			Stack<int> st2 = new(h2);
+			Stack<int> st3 = new(h3);
+			while (sumSt1 != sumSt2 || sumSt2 != sumSt3)
+			{
+				if (sumSt1 > sumSt2 && sumSt1 > sumSt3)
+				{
+					sumSt1 -= st1.Pop();
+				}
+				else if (sumSt2 > sumSt3)
+				{
+					sumSt2 -= st2.Pop();
+				}
+				else
+				{
+					sumSt3 -= st3.Pop();
+				}
+			}
+			return sumSt1;
+		}
+	}
+}
